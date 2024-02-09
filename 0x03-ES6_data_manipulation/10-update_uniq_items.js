68983821c54d1acbe11c60fe9d86944a61b5f776
Pinkypre <preciouspncube@gmail.com>
2024-02-09 22:20:09 +0000

    task

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-param-reassign */
 export default function updateUniqueItems(map) {
-    if (map instanceof Map) {
-      for (const [key, value] of map) {
-        if (value === 1) {
-          map.set(key, 100);
-        }
-      }
-  
-      return map;
-    }
-  
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
