052919a46871ce89114a6757cb200e334370969d
Pinkypre <preciouspncube@gmail.com>
2024-02-09 22:42:50 +0000

    tasks

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,11 +1,7 @@
-/**
- * Application controller
- */
 class AppController {
-  static getHomepage(request, response) {
-    response.statusCode = 200;
-    response.send('Hello Holberton School!');
+    static getHomepage(request, response) {
+      response.send(200, 'Hello Holberton School!');
+    }
   }
-}
-
-export default AppController;
+  
+  export default AppController;
