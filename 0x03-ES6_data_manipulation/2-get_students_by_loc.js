333d2b6a6aa1b5dbe14ba7f4aa3c7f85783be78f
Pinkypre <preciouspncube@gmail.com>
2024-02-09 21:38:57 +0000

    tasks

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,8 @@
-export default function getStudentsByLocation(list, city) {
-    return list.filter((obj) => obj.location === city);
-  }
+export default function getStudentsByLocation(A, B) {
+    const result = A.filter((a) => {
+        if (a.location === B) {
+            return a;
+        }
+    })
+    return result;
+}
