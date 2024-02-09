4fe5bfae1bb0bb7dbf5bdbc35565ce81cae3f729
Pinkypre <preciouspncube@gmail.com>
2024-02-09 21:48:23 +0000

    task

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,7 @@
-export default function getStudentIdsSum(list) {
-    return list.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
-  }
+export default function getStudentIdsSum(A) {
+    const arr = A.map((d) => {return d.id;});
+    const result = arr.reduce((a, b) => {
+        return a + b;
+    })
+    return result;
+}
